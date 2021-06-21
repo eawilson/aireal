af759cdcc3423daad53d80bd1612a57efbd1bb9e
Ed Wilson <edwardadrianwilson@yahoo.co.uk>
2021-06-22 00:46:13 +0100

    Fix typo in init_db script

--- aireal/scripts/init_db.py
+++ aireal/scripts/init_db.py
@@ -33,16 +33,15 @@ def init_db(instance_path, no_data):
             with trans.cursor() as cur:
 
                 # Delete all tables of old database
-                if command == "init":
-                    sql = """DO $$ DECLARE
-                                 r RECORD;
-                             BEGIN
-                                 FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
-                                     EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
-                                 END LOOP;
-                             END $$;
-                             
-                             DROP TYPE IF EXISTS movability;"""
+                sql = """DO $$ DECLARE
+                                r RECORD;
+                            BEGIN
+                                FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
+                                    EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+                                END LOOP;
+                            END $$;
+                            
+                            DROP TYPE IF EXISTS movability;"""
                     cur.execute(sql)
                     trans.commit()
 
